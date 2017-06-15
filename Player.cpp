0e17eb5878bb3cafc81aa2480e59d20d4f2aa0e2
macfisher <macfisher01@gmail.com>
2017-06-15 08:27:06 -0600

    Added a code branching example using C++ enum

--- Player.cpp
+++ Player.cpp
@@ -76,4 +76,51 @@ int main() {
 	} else {
 	cout << num2 << endl;
 	}
+
+	// Else if example /w enum
+	enum Item { // Type which values are restricted to specific range of values
+		Coin, Key, Sanddollar // Variables that type Item can have
+	};
+
+	cout << " ,------------------." << endl;
+	cout << " |                  |" << endl;
+	cout << " | Choose an item:  |" << endl;
+	cout << " |   1. Key         |" << endl;
+	cout << " |   2. Coin        |" << endl;
+	cout << " |   3. Sanddollar  |" << endl;
+	cout << " |                  |" << endl;
+	cout << " `------------------'" << endl;
+
+	char player_item_choice;
+	cin >> player_item_choice;
+
+	Item itemInHand;
+
+	switch (player_item_choice) {
+	case '1':
+		itemInHand = Key;
+		break;
+	case '2':
+		itemInHand = Coin;
+		break;
+	case '3':
+		itemInHand = Sanddollar;
+		break;
+	default:
+		cout << "Invalid selection chosen" << endl;
+		break;
+	}
+	
+	if (itemInHand == Key) {
+		cout << "The key has a lionshead on the handle." << endl;
+		cout << "You got into a secret room using the key!" << endl;
+	} else if (itemInHand == Coin) {
+		cout << "The coin is a rusted brassy color. It has a picture of a lady with a skirt." << endl;
+		cout << "Using this coin you could buy a few things" << endl;
+	} else if (itemInHand == Sanddollar) {
+		cout << "The sanddollar has a little star on it." << endl;
+		cout << "You might be able to trade it for something." << endl;
+	}
+
+	return 0;
 }
